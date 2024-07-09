13ee1e8123ffaf5d2143b641e11b0f35a4092912
Jailton Monteiro <jailtonmonteiro147@gmail.com>
2024-07-09 20:04:07 -0300

    Comparação com uso de Equals e GetHashCode.

---
+++ HashCodeEquals/Program.cs
@@ -0,0 +1,18 @@
+﻿using HashCodeEquals.Entities;
+
+namespace HashCodeEquals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client() { Name = "Maria", Email = "maria@gmail.com" };
+            Client b = new Client() { Name = "Alex", Email = "Alex@gmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a == b);
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
